919baa7f0d465882b29c63d09ad523e711e10393
kunalmishra5 <41867984+kunalmishra5@users.noreply.github.com>
2019-01-10 20:08:35 +0530

    Update __init__.py

--- pystache/__init__.py
+++ pystache/__init__.py
@@ -8,6 +8,6 @@ TODO: add a docstring.
 
 from pystache.init import parse, render, Renderer, TemplateSpec
 
-__all__ = ['parse', 'render', 'Renderer',,, 'TemplateSpec']
+__all__ = ['parse', 'render', 'Renderer', 'TemplateSpec']
 
 __version__ = '0.5.3'  # Also change in setup.py.
